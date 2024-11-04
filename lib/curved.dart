3255195f9f67ffc19067fe9bd097b3461efd9682
MuhammadYousufbhimla <Muhammadyousufgemin@gmail.com>
2024-11-04 23:52:05 +0500

    bottom navigation bar

--- lib/curved.dart
+++ lib/curved.dart
@@ -9,11 +9,13 @@ class Curved extends StatefulWidget {
   @override
   State<Curved> createState() => _CurvedState();
 }
-int _page = 0;
-GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
-var pages = [Signup(),Signin(name: ''),Signup(),AboutPage(),Signup()];
+//int _page = 0;
 
-  int _selectedItem=0;
+GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
+             //0        //1             //2      //3        // 4
+var pages = [Signup(),Signin(name: ''),Signup(),AboutPage(),Signup()];
+ 
+
 class _CurvedState extends State<Curved> {
   @override
   Widget build(BuildContext context) {
@@ -21,38 +23,41 @@ class _CurvedState extends State<Curved> {
         bottomNavigationBar: CurvedNavigationBar(
           key: _bottomNavigationKey,
           index: 0,
-          items: <Widget>[
-            Icon(Icons.add, size: 30),
+          items: 
+          [
+            Icon(Icons.add, size: 30,color: Colors.green,),
             Icon(Icons.list, size: 30),
             Icon(Icons.compare_arrows, size: 30),
             Icon(Icons.call_split, size: 30),
             Icon(Icons.perm_identity, size: 30),
+            
           ],
           color: Colors.white,
-          buttonBackgroundColor: Colors.white,
-          backgroundColor: Colors.blueAccent,
-          animationCurve: Curves.easeInOut,
+          buttonBackgroundColor: const Color.fromARGB(255, 209, 75, 75),
+          backgroundColor: const Color.fromARGB(240, 82, 123, 196),
+          animationCurve: Curves.bounceIn,
           animationDuration: Duration(milliseconds: 600),
           
           onTap: (index) {
+            print(index);
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => pages[index]));
 
             // setState(() {
             //   _page = index;
-            // });
-            
+            // });\
           },
-        
           letIndexChange: (index) => true,
         ),
         body: Container(
-          color: Colors.blueAccent,
+        //  color: Colors.blueAccent,
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(_page.toString(), style: TextStyle(fontSize: 160)),
+              children: <Widget>
+              [
+                Text("0",style: TextStyle(fontSize: 160)),
+               // Text(_page.toString(), style: TextStyle(fontSize: 160)),
                 ElevatedButton(
                   child: Text('Go To Page of index 1'),
                   onPressed: () {
@@ -62,6 +67,7 @@ class _CurvedState extends State<Curved> {
                   },
                 )
               ],
+              
             ),
           ),
         )
